992d6a143737ac3e273eb24d7acc2efd5356f0e4
Rmiverson <Rmiverson99@gmail.com>
2020-11-18 16:27:58 -0800

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+   attr_accessor :roster
+
+   @@all = []
+
+   def initialize(name)
+      @name = name
+      @roster = {}
+      @@all << self
+   end
+
+   def self.all
+      @@all
+   end
+
+   def add_student(student_name, student_grade)
+      if !@roster[student_grade]
+         @roster[student_grade] = []
+      end
+      @roster[student_grade] << student_name
+   end
+
+   def grade(student_grade)
+      @roster[student_grade]
+   end
+
+   def sort
+      ans = {}
+      @roster.map do|grades, classes|
+         ans[grades] ||= classes.sort
+      end
+      ans
+   end
+end
